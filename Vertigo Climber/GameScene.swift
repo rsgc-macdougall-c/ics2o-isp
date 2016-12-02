39c7d21ba04fef8e9d890283ae2605549e6ea2b3
Connor MacDougall <Connor@macbook-pro-870-macdougall.local>
2016-12-02 17:40:44 -0500

    Polishing and editing current code. Nothing new added

--- Vertigo Climber/GameScene.swift
+++ Vertigo Climber/GameScene.swift
@@ -17,14 +17,23 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
-       //lets add a background sprite
+       //adds the background sprite
         let background = SKSpriteNode (imageNamed: "background")
-        background.position = CGPoint(x: size.width / 2, y: size.height / 2)//the background is now anchored in the middle of the screen
+        
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        
+        //the background is now anchored in the middle of the screen
+        
         background.size = self.frame.size //set the size to the size of the screen
+        
+        background.zPosition = -1
         addChild(background)  //add the background sprite to the scene graph
         
-        ninja.position = CGPoint(x: size.width/2, y: 400) // (0,0) is the bottom left
-        addChild(ninja) //add the santa sprite to the scene
+        ninja.position = CGPoint(x: size.width/2, y: 400)
+        
+        // (0,0) is the bottom left of the screen
+        
+        addChild(ninja) //adds the ninja sprite to the scene
         
     }
 }
--- Vertigo Climber/GameViewController.swift
+++ Vertigo Climber/GameViewController.swift
@@ -14,42 +14,12 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+        let skView = self.view as! SKView
+        skview.showsFPS = true
+        skview.showsNodeCounter = true
+        
+        
+    
 }
