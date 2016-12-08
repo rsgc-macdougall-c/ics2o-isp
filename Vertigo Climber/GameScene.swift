853e56951fcd1aad9537667aa30f2fb805100a1f
Connor MacDougall <Connor@macbook-pro-870-macdougall.local>
2016-12-08 10:52:27 -0500

    I have one of my two actions completed, need to make an action to go to the opposite side. Also added the sprites that act as the walls but they will not load in properly.

--- Vertigo Climber/GameScene.swift
+++ Vertigo Climber/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     
     // adding a "Hero" sprite
     let ninja = SKSpriteNode(imageNamed: "ninja")
+    let bambooShoot = SKSpriteNode(imageNamed: "bamboo shoot")
+    let bambooShootTwo = SKSpriteNode(imageNamed: "bamboo shoot two")
     
     
     override func didMove(to view: SKView) {
@@ -36,6 +38,40 @@ class GameScene: SKScene {
         
         addChild(ninja) //adds the ninja sprite to the scene
         
+        bambooShoot.position = CGPoint(x: 263, y: size.height/2)
+        
+        addChild(bambooShoot)
+        
+        bambooShootTwo.position = CGPoint(x: 1273, y: size.height/2)
+        
+        addChild(bambooShootTwo)
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //manage finger size on screen
+        guard let touch = touches.first else {
+            return
+        }
+        
+        //get the location of the touch
+        let touchLocation = touch.location(in: self)
+       
+        //Print the location to the console
+        print(touchLocation.x)
+        print(touchLocation.y)
+        
+        let destination = CGPoint(x: 450, y: ninja.position.y)
+        
+        let actionMove = SKAction.move(to: destination, duration: 0.33)
+        
+        //tell ninja to move
+        ninja.run(actionMove)
+
+        
+    }
+    
+    
 }
     
